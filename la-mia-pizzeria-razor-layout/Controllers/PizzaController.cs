8f95933ad3baf2de44bb82cfc314c9e1e6efac41
luigitarallo <tarallo.gigi@gmail.com>
2024-05-15 14:32:27 +0200

    add PizzaController

---
+++ la-mia-pizzeria-razor-layout/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_razor_layout.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
