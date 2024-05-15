daee282cf11c7495537673e58d1640ba6dda7bde
luigitarallo <tarallo.gigi@gmail.com>
2024-05-15 15:07:35 +0200

    add PizzaContext

---
+++ la-mia-pizzeria-razor-layout/Data/PizzaContext.cs
@@ -0,0 +1,14 @@
+﻿using la_mia_pizzeria_razor_layout.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_razor_layout.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_pizza;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
