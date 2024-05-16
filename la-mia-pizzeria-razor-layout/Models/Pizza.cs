c31e8c75833fb263a989feb067443997ba4b3a7b
luigitarallo <tarallo.gigi@gmail.com>
2024-05-16 17:53:31 +0200

    add create, PriceAttribute

--- la-mia-pizzeria-razor-layout/Models/Pizza.cs
+++ la-mia-pizzeria-razor-layout/Models/Pizza.cs
@@ -9,9 +9,18 @@ namespace la_mia_pizzeria_razor_layout.Models
     public class Pizza
     {
         [Key] public int PizzaId { get; set; }
-        [Required] public string Name { get; set; }
+        [Required(ErrorMessage ="Pizza name is mandatory")]
+        [StringLength(25, ErrorMessage = "Pizza name has to be under 25 letters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Pizza description is mandatory")]
+
         public string Description { get; set; }
+
+        [Required(ErrorMessage ="Pizza photo is mandatory")]
         public string Photo { get; set; }
+
+        [Price(ErrorMessage = "Invalid price format or value.")]
         public decimal Price { get; set; }
 
         public Pizza() { }
---
+++ la-mia-pizzeria-razor-layout/Models/PriceAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class PriceAttribute : ValidationAttribute
+{
+    public decimal Price { get; set; }
+    public PriceAttribute() { }
+    public PriceAttribute(int price)
+    {
+        this.Price = price;
+    }
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        decimal fieldValue = (decimal)value;
+
+        if (fieldValue == null || fieldValue < 0)
+        {
+            return new ValidationResult($"Insert a valid price");
+        }
+
+        return ValidationResult.Success;
+    }
+}
+
+
